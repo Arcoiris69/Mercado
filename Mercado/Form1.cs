5984e089512f5300fe03da7d159b7f41aa9646b2
Luis Fernando Espindola Silva <lespindola@ucol.mx>
2018-04-18 21:27:00 -0500

    Agregar archivos de proyecto.

---
+++ Mercado/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mercado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Producto prod;
+        Inventario inv = new Inventario();
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            prod = new Producto(txtCodigo.Text, txtNombre.Text, txtDescripcion.Text,Convert.ToInt32(txtCantidad.Text), Convert.ToInt32(txtCosto.Text));
+            inv.AddProduct(prod);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtMonitor.Text = inv.ListAllProducts();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+                inv.DeleteProduct(Convert.ToInt32(txtPosition.Text));
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+           txtMonitor.Text =  inv.SearchProduct(txtPosition.Text).ToString();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            prod = new Producto((txtCodigo.Text), txtNombre.Text, txtDescripcion.Text, Convert.ToInt32(txtCantidad.Text), Convert.ToInt32(txtCosto.Text));
+            inv.insertProduct(prod,Convert.ToInt32(txtPosition.Text));
+        }
+    }
+}
